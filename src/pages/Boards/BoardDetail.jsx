412bf20cc7380982ef60f39a1236b71771e41e79
anhquan2211 <anhquanst2211@gmail.com>
2024-01-07 22:44:17 +0700

    refactor project structure

---
+++ src/pages/Boards/BoardDetail.jsx
@@ -0,0 +1,19 @@
+import Container from '@mui/material/Container'
+
+import AppBar from '../../components/AppBar'
+import BoardBar from './components/BoardBar'
+import BoardContent from './components/BoardContent'
+
+function BoardDetail() {
+  return (
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <AppBar />
+
+      <BoardBar />
+
+      <BoardContent />
+    </Container>
+  )
+}
+
+export default BoardDetail
