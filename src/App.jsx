ea99030455c934f4e8cd364b479af08fd4752fd8
anhquan2211 <anhquanst2211@gmail.com>
2024-01-07 21:59:40 +0700

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import theme from './theme'
+
 function App() {
-  return <div>Hello</div>
+  return (
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <Box
+        sx={{
+          backgroundColor: 'primary.light',
+          width: '100%',
+          height: (theme) => theme.trello.appBarHeight,
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      ></Box>
+      <Box
+        sx={{
+          backgroundColor: 'primary.dark',
+          width: '100%',
+          height: (theme) => theme.trello.boardBarHeight,
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        Board Bar
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: 'primary.main',
+          width: '100%',
+          height: (theme) =>
+            `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        Board Content
+      </Box>
+    </Container>
+  )
 }
 
 export default App
