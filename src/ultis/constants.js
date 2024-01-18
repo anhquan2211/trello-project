363e964c7afa9b33a5ad2d900c55b307c61c6eed
anhquan2211 <anhquanst2211@gmail.com>
2024-01-19 00:12:18 +0700

    fix BUILD_MODE to vercel

--- src/ultis/constants.js
+++ src/ultis/constants.js
@@ -1,2 +1,12 @@
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8080'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-whg9.onrender.com'
+}
 // export const API_ROOT = 'http://localhost:8080'
-export const API_ROOT = 'https://trello-api-whg9.onrender.com'
+// export const API_ROOT = 'https://trello-api-whg9.onrender.com'
+
+export const API_ROOT = apiRoot
