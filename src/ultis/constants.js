1d6d50d3c3e90bbb54955cb558287b00f5609964
anhquan2211 <anhquanst2211@gmail.com>
2024-01-20 19:43:53 +0700

    change white-list-domains

--- src/ultis/constants.js
+++ src/ultis/constants.js
@@ -4,7 +4,8 @@ if (process.env.BUILD_MODE === 'dev') {
 }
 
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://trello-api-whg9.onrender.com'
+  // apiRoot = 'https://trello-api-whg9.onrender.com',
+  apiRoot = 'https://trello-api.anhquandev.com'
 }
 // export const API_ROOT = 'http://localhost:8080'
 // export const API_ROOT = 'https://trello-api-whg9.onrender.com'
