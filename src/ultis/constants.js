70df35b7aa7d0fc136adc35a8a4abd7a80794fb1
quanla <luuanhquan@gmail.com>
2024-04-20 08:37:05 +0700

    add domain whitelist

--- src/ultis/constants.js
+++ src/ultis/constants.js
@@ -8,6 +8,6 @@ if (process.env.BUILD_MODE === 'production') {
   // apiRoot = 'https://trello-api.anhquandev.com'
 }
 // export const API_ROOT = 'http://localhost:8080'
-export const API_ROOT = 'https://trello-api-guvt.onrender.com'
+// export const API_ROOT = 'https://trello-api-guvt.onrender.com'
 
-// export const API_ROOT = apiRoot
+export const API_ROOT = apiRoot
